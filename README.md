a967ff3ae820f4644b6046f94927554de0930b48
unknown <amv.harsh@gmail.com>
2022-11-12 11:33:02 +0530

    added usefull file in the repo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PortfolioWebsite
 this is my portfolio website 
+i made it using 
+html/html5
+css/css3
+javascript
+jQuery
---
+++ index.js
@@ -0,0 +1,23 @@
+// loading animation 
+
+function fun(){
+    
+    var elm=document.querySelector("#cardup").classList.add("anim")
+}
+
+function func(){
+    var elm=document.querySelector("#cardup").classList.remove("anim")
+
+}
+function fun1(){
+    
+    var elm=document.querySelector("#cardup1").classList.add("anim1")
+}
+
+function func1(){
+    var elm=document.querySelector("#cardup1").classList.remove("anim1")
+
+}
+
+
+
